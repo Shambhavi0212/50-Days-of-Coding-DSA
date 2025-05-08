a30bab0ddf6c5664695596b3eb1dc987b58be3b9
Vallapi Shambhavi <vallapishambhavi02@gmail.com>
2025-05-08 20:33:38 +0530

    Day 7/50: Added solved problem and screenshots

---
+++ Day7/DifficultyRatingOrder.java
@@ -0,0 +1,83 @@
+/*
+Difficulty Rating Order
+
+Our Chef has some students in his coding class who are practicing problems.
+Given the difficulty of the problems that the students have solved in order,
+help the Chef identify if they are solving them in non-decreasing order of difficulty.
+
+Non-decreasing means that the values in an array is either increasing or remaining the same, but not decreasing.
+That is, the students should not solve a problem with difficulty d1, and then later a problem with difficulty d2, where d1 > d2.
+
+Output “Yes” if the problems are attempted in non-decreasing order of difficulty rating and “No” if not.
+
+Input Format:
+- The first line of input will contain a single integer T, denoting the number of test cases.
+- The description of the test cases follows.
+- Each test case consists of 2 lines of input:
+  - The first line contains a single integer N, the number of problems solved by the students.
+  - The second line contains N space-separated integers, the difficulty ratings of the problems attempted by the students in order.
+
+Output Format:
+- For each test case, output in a new line “Yes” if the problems are attempted in non-decreasing order of difficulty rating and “No” if not.
+- The output should be printed without the quotes.
+- Each letter of the output may be printed in either lowercase or uppercase.
+
+Constraints:
+1 ≤ T ≤ 100
+2 ≤ N ≤ 100
+1 ≤ difficulty of each problem ≤ 5000
+
+Sample Input:
+4
+3
+1 2 3
+3
+1 1 2
+5
+100 200 300 400 350
+5
+1000 2000 5000 3000 1000
+
+Sample Output:
+Yes
+Yes
+No
+No
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+         
+
+        while (t-- > 0) {
+            int flag=0;
+            int n = scanner.nextInt();
+            int[] d = new int[n];
+            
+            for (int i = 0; i < n; i++) {
+                d[i] = scanner.nextInt();
+            }
+           
+            for(int i=0;i<n-1;i++)
+            {
+                if(d[i]>d[i+1])
+                {
+                    flag=1;
+                }
+             
+            }
+             if(flag==1)
+        {
+            System.out.println("No");
+        }
+        else
+        {
+            System.out.println("Yes");
+        }
+        }
+        
+    }
+}
